02b20b30817a78d76cbd1f4f89a04df47de9005c
Ryan Crowley <ryancrowleycode@gmail.com>
2020-01-30 15:41:50 -0600

    working on exercises

--- musichistory.sql
+++ musichistory.sql
@@ -2,10 +2,29 @@
 SELECT * FROM Genre;
 
 -- Using the INSERT statement, add one of your favorite artists to the Artist table.
+INSERT INTO Artist (ArtistName, YearEstablished)
+VALUES ("John Mayer", 1999);
+
+SELECT * FROM Artist;
 
 -- Using the INSERT statement, add one, or more, albums by your artist to the Album table.
+INSERT INTO Album (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId)
+VALUES ("Continuum", "09/12/2006", 2974, "Columbia", 28, 2);
+
+SELECT * FROM Album;
+
 
 -- Using the INSERT statement, add some songs that are on that album to the Song table.
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+VALUES ("Waiting On The World To Change", 201, "09/12/2006", 2, 28, 24);
+
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+VALUES ("I Don't Trust Myself(With Loving You)", 292, "09/12/2006", 2, 28, 24);
+
+INSERT INTO Song (Title, SongLength, ReleaseDate, GenreId, ArtistId, AlbumId)
+VALUES ("Belief", 242, "09/12/2006", 2, 28, 24);
+
+SELECT * FROM Song;
 
 -- Write a SELECT query that provides the song titles, album title, and artist name for all of the data you just entered in. Use the LEFT JOIN keyword sequence to connect the tables, and the WHERE keyword to filter the results to the album and artist you added.
 
